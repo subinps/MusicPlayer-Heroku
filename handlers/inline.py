c477ce96fe6540cc545cbc5a27d4cb2928264e00
subinps <64341611+subinps@users.noreply.github.com>
2021-03-07 11:06:15 +0530

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -32,7 +32,7 @@ async def search(client: Client, query: InlineQuery):
                         v["viewCount"]["short"]
                     ),
                     input_message_content=InputTextMessageContent(
-                        "https://www.youtube.com/watch?v={}".format(
+                        "/play https://www.youtube.com/watch?v={}".format(
                             v["id"]
                         )
                     ),
